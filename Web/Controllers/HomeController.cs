0ded40800b7f6395fb5a18278a58a379ececb763
Cristoffer Andersson <cristoffer@ezy.se>
2017-01-11 08:36:24 +0100

    Add check for content type when parsing in client. Fallback to Json if Protobuf is not available

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Infrastructure.Model;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Web.Controllers
 {
@@ -13,14 +14,23 @@ namespace Web.Controllers
         {
             var client = new System.Net.Http.HttpClient { BaseAddress = new Uri("http://localhost:5002") };
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             var response = await client.GetAsync("api/home");
             if (response.IsSuccessStatusCode)
             {                
                 using(var stream = await response.Content.ReadAsStreamAsync())
                 {
-                    var model = ProtoBuf.Serializer.Deserialize<ProtobufModelDto>(stream);
-                    return $"{model.Name}, {model.StringValue}, {model.Id}";
+                    ProtobufModelDto model = null;
+                    if (response.Content.Headers.ContentType.MediaType == "application/x-protobuf")
+                    {
+                        model = ProtoBuf.Serializer.Deserialize<ProtobufModelDto>(stream);
+                        return $"{model.Name}, {model.StringValue}, {model.Id}";
+                    }
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<ProtobufModelDto>(content);
+                    return $"{model.Name}, {model.StringValue}, {model.Id}";  
                 }
             }
 
